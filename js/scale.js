13fcf5a6aa63ea99227b2c6e9f72e877b055a53d
yg15637480263 <583675687@qq.com>
2017-09-26 14:00:28 +0800

    first

---
+++ js/scale.js
@@ -0,0 +1,13 @@
+﻿(function (doc, win) {
+		        var docEle = doc.documentElement;
+		       	var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
+		        var recalCulate = function () {
+		            var width = docEle.clientWidth;
+		            docEle.style.fontSize = 100 * (width / 640) + 'px';
+		        };
+		
+		        recalCulate();
+		
+		        if (!doc.addEventListener) return;
+		        win.addEventListener(resizeEvent, recalCulate, false);
+		    })(document, window);
